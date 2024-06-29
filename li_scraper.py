aae8932af4b1ea6a9da40cb57b4c4b59194bdce7
nate2427 <natebaker2427@gmail.com>
2024-06-28 20:42:01 -0400

    added apify functionality.

--- li_scraper.py
+++ li_scraper.py
@@ -1,21 +1,55 @@
-from apify_client import ApifyClient
+from dotenv import load_dotenv
+import os, requests
+import pprint
+load_dotenv()
 
-# Initialize the ApifyClient with your API token
-client = ApifyClient("<YOUR_API_TOKEN>")
+APIFY_API_KEY = os.getenv("APIFY_API_KEY")
+ACTOR_RUN_URL = f"https://api.apify.com/v2/acts/kfiWbq3boy3dWKbiL/runs?token={APIFY_API_KEY}"
 
-# Prepare the Actor input
-run_input = {
-    "searchUrl": None,
+
+
+print("Client initialized")
+
+# read in cookie from cookie.json
+import json
+with open("cookie.json") as f:
+    cookie = json.load(f)
+
+def run_linkedin_scraper(profile_url):
+    # Prepare the Actor input
+    run_input = {
+    "cookie": cookie,
     "deepScrape": True,
-    "strictMode": False,
-    "startPage": 1,
-    "minDelay": 2,
+    "filters.fromMembers": [
+        profile_url
+    ],
     "maxDelay": 5,
-}
+    "minDelay": 2,
+    "startPage": 1,
+    "endPage": 1,
+    "strictMode": False
+    }
+    response = requests.post(ACTOR_RUN_URL, json=run_input)
+    data = response.json()
+    return data["data"]["defaultDatasetId"]
+    
 
-# Run the Actor and wait for it to finish
-run = client.actor("kfiWbq3boy3dWKbiL").call(run_input=run_input)
+def get_run_results(dataset_id):
+    ACTOR_GET_RUN_URL = f"https://api.apify.com/v2/datasets/{dataset_id}/items?token={APIFY_API_KEY}"
+    response = requests.get(ACTOR_GET_RUN_URL)
+    print(ACTOR_GET_RUN_URL)
+    data = response.json()
+    # data is a list of objects. extract from each object the url, text, timeSincePosted and return that object
+    extracted_data = []
+    for item in data:
+        extracted_item = {
+            "url": item["url"],
+            "text": item["text"],
+            "timeSincePosted": item["timeSincePosted"]
+        }
+        extracted_data.append(extracted_item)
+    
+    return extracted_data
 
-# Fetch and print Actor results from the run's dataset (if there are any)
-for item in client.dataset(run["defaultDatasetId"]).iterate_items():
-    print(item)
+if __name__ == "__main__":
+    pprint.pprint(get_run_results("ed29KDzMpkOznXPRH"))
